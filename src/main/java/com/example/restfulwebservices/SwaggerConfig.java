c5026ab78902166c16c3258b373417a0b42be6c5
Arvind Verma <arvind.verma@hk.ey.com>
2017-12-30 12:55:32 +0800

    Swagger changes

--- src/main/java/com/example/restfulwebservices/SwaggerConfig.java
+++ src/main/java/com/example/restfulwebservices/SwaggerConfig.java
@@ -9,6 +9,9 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 @Configuration
 @EnableSwagger2
@@ -16,10 +19,13 @@ public class SwaggerConfig {
 
     public static final Contact DEFAULT_CONTACT = new Contact("Arvind Verma", "", "arvindcse.verma@gmail.com");
     private static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Api Documentation", "User Profile Title", "1.0", "urn:tos", DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0", new ArrayList());;
+    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<>(Arrays.asList("application/json", "application/xml"));
 
     @Bean
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(DEFAULT_API_INFO);
+                .apiInfo(DEFAULT_API_INFO)
+                .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+                .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
     }
 }
